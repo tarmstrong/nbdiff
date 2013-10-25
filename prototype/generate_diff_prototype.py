faa85490c8c621a76f79ccde2866ca274c4d023a
Tavish Armstrong <tavisharmstrong@gmail.com>
2013-10-25 14:47:12 -0400

    Add staging checkboxes to nbdiff prototype

--- prototype/generate_diff_prototype.py
+++ prototype/generate_diff_prototype.py
@@ -6,18 +6,30 @@ import BeautifulSoup as b
 first_cells = []
 for i in range(15):
     with open('cells/cell-1-{i:d}.html'.format(i=i)) as f:
-        first_cells.append(f.read())
+        cell_content = f.read()
+        if len(cell_content) > 0:
+            first_cells.append(cell_content)
 
 second_cells = []
 for i in range(15):
     with open('cells/cell-2-{i:d}.html'.format(i=i)) as f:
-        second_cells.append(f.read())
+        cell_content = f.read()
+        if len(cell_content) > 0:
+            second_cells.append(cell_content)
 
 zcells = zip(first_cells, second_cells)
 
-rowfmt = "<div class='row'><div class='row-cell'>{}</div><div class='row-cell'>{}</div></div>"
+# for some reason the middle cell has to be at the bottom? whyyy?
+rowfmt = '''
+<div class='row'>
+<div class='row-cell-diff-left'>{}</div>
+<div class='row-cell-diff-right'>{}</div>
+<div class='row-cell-diff-middle'>
+<input type='checkbox' data-cell-idx='{idx:d}' class='staging-checkbox' checked />
+</div>
+</div>'''
 
-rendered_cells = "\n".join([rowfmt.format(a, b) for a, b in zcells])
+rendered_cells = "\n".join([rowfmt.format(a, b, idx=i) for i, (a, b) in enumerate(zcells)])
 
 rendered_full = Template(open('nbdiff-template.html').read()).render(cells=rendered_cells)
 
