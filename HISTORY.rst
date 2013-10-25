17cf35cee89536dd2714f960330abf31125d35f3
Tavish Armstrong <tavisharmstrong@gmail.com>
2013-10-25 09:05:02 -0400

    Creating basic PyPI package layout.

---
+++ HISTORY.rst
@@ -0,0 +1,9 @@
+.. :changelog:
+
+History
+-------
+
+0.1.0 (2013-11-25)
+++++++++++++++++++
+
+* First release on PyPI.
---
+++ tests/test_nbdiff.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_nbdiff
+----------------------------------
+
+Tests for `nbdiff` module.
+"""
+
+import unittest
+
+from nbdiff import nbdiff
+
+
+class TestNbdiff(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
