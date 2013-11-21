b8f6cdd5b2665ffcf9c397771e39d9a60992e98d
Tavish Armstrong <tavisharmstrong@gmail.com>
2013-11-21 11:34:03 -0500

    Diff command works.

--- nbdiff/commands.py
+++ nbdiff/commands.py
@@ -2,8 +2,10 @@
 Entry points for the nbdiff package.
 '''
 import argparse
-from notebook_parser import NotebookParser
-from comparable import CellComparator
+from .notebook_parser import NotebookParser
+from .comparable import CellComparator
+from . import diff as differ
+import json
 
 
 def diff():
@@ -19,16 +21,14 @@ def diff():
         notebook1 = x.parse(args.notebook[0])
         notebook2 = x.parse(args.notebook[1])
         notequal = []
-        for i in range(0, len(notebook1["worksheets"][0]["cells"])):
-            cell1 = notebook1['worksheets'][0]['cells'][i]
-            cell2 = notebook2['worksheets'][0]['cells'][i]
-            if not CellComparator(cell1) == CellComparator(cell2):
-                notequal.append((cell1, cell2))
-        for cell1, cell2 in notequal:
-            print(cell1)
-            print(cell2)
-
-    print('Arguments received: {}'.format(args.notebook))
+        nb1_cells = [CellComparator(c) for c in notebook1['worksheets'][0]['cells']]
+        nb2_cells = [CellComparator(c) for c in notebook2['worksheets'][0]['cells']]
+        result = differ.diff(nb1_cells, nb2_cells)
+        notebook1['worksheets'][0]['cells'] = [
+            dict(r['value'].data.items() + {'metadata': {'state': r['state']}}.items())
+            for r in result
+        ]
+        print json.dumps(notebook1)
 
 
 def merge():
