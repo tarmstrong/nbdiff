6180ec3fab35e0af1e13e7573f758edb2e5b7f8b
Tavish Armstrong <tavisharmstrong@gmail.com>
2013-11-21 20:48:30 -0500

    Fix nbmerge to use git show in the no-arguments case.

--- nbdiff/notebook_parser.py
+++ nbdiff/notebook_parser.py
@@ -6,7 +6,6 @@ import json
 class NotebookParser:
 
     def parse(self, json_data):
-        json_data = open(json_data)
         data = json.load(json_data)
         json_data.close()
         return data
