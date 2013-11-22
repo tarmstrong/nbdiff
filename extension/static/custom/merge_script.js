0683b4fa81e78ebe724c2d092e8178a1d5392cae
Tavish Armstrong <tavisharmstrong@gmail.com>
2013-11-21 22:24:55 -0500

    Spaces instead of tabs in merge_script.js

--- extension/static/custom/merge_script.js
+++ extension/static/custom/merge_script.js
@@ -1,52 +1,52 @@
 $( document ).ready(function() {
 
-	var cellState = ['added', 'deleted', 'modified', 'unchanged', 'empty'];
-	var cellSide = ['local', 'base', 'remote'];
-	var cells = IPython.notebook.get_cells();
-	
-	//The indexing for IPython.notebook.get_cells_element(index) messes up with append in 
-	//generate_merge_column so this data-structure was created to preserve index.
-	var cellElements = [];
-	for(var i = 0; i < cells.length; i++){
-		cellElements[i] = IPython.notebook.get_cell_element(i);
-	}
-	
-	var init = function(){
+    var cellState = ['added', 'deleted', 'modified', 'unchanged', 'empty'];
+    var cellSide = ['local', 'base', 'remote'];
+    var cells = IPython.notebook.get_cells();
+    
+    //The indexing for IPython.notebook.get_cells_element(index) messes up with append in 
+    //generate_merge_column so this data-structure was created to preserve index.
+    var cellElements = [];
+    for(var i = 0; i < cells.length; i++){
+        cellElements[i] = IPython.notebook.get_cell_element(i);
+    }
+    
+    var init = function(){
     var cells = IPython.notebook.get_cells();
     console.log('Initializing nbdiff.');
     if (cells.length > 0 && typeof cells[0].metadata.state !== 'undefined') {
-					$('#notebook-container').css("visibility", "hidden");
+                    $('#notebook-container').css("visibility", "hidden");
       console.log('Found nbdiff metadata in the notebook.');
-						console.log('Hiding the normal notebook container.');
+                        console.log('Hiding the normal notebook container.');
         $('#notebook-container').hide();
-						var cssLink = $("<link rel='stylesheet' type='text/css' href='/static/custom/merge.css'>");
-						$("head").append(cssLink); 
+                        var cssLink = $("<link rel='stylesheet' type='text/css' href='/static/custom/merge.css'>");
+                        $("head").append(cssLink); 
       console.log('Creating a new notebook container.');
       $('#notebook').append(generate_notebook_container());
 
       console.log('Initializing merge rows.');
       init_notebook_merge_rows();
     }
-				else {
-					console.log('No nbdiff metadata in the notebook.');
-					console.log('Showing the normal notebook container.');
-				}
-	};
-	
-	var init_notebook_merge_rows = function(){
-		
-		for (var i = 0; i < cells.length; i++) {
+                else {
+                    console.log('No nbdiff metadata in the notebook.');
+                    console.log('Showing the normal notebook container.');
+                }
+    };
+    
+    var init_notebook_merge_rows = function(){
+        
+        for (var i = 0; i < cells.length; i++) {
       console.log('Processing cell #' + i + '...');
-			parse_merge_row( cells[i], i);
-		}
-		$('#notebook-container-new').append(generate_notebook_container_end_space());
-	};
-	
-	var parse_merge_row = function(cell, index){
-		var side = cell.metadata.side;
-		var state = cell.metadata.state;
-		
-		if (side === cellSide[0]){
+            parse_merge_row( cells[i], i);
+        }
+        $('#notebook-container-new').append(generate_notebook_container_end_space());
+    };
+    
+    var parse_merge_row = function(cell, index){
+        var side = cell.metadata.side;
+        var state = cell.metadata.state;
+        
+        if (side === cellSide[0]){
       console.log('New row. Adding local cell.');
       var new_row = $(generate_empty_merge_row());
       new_row.find("input.merge-arrow-right").click(function(index, state, row) {
@@ -73,12 +73,12 @@ $( document ).ready(function() {
         };
       }(index, state, new_row));
         
-			$('#notebook-container-new').append(new_row);
-		}		
+            $('#notebook-container-new').append(new_row);
+        }        
     else {
       console.log('Adding ' + side + ' cell.');
     }
-		generate_merge_column(side, state, index);		
+        generate_merge_column(side, state, index);        
 
     var current_row = $("#notebook-container-new").children().last();
     if (state === cellState[3] || state === cellState[4]) {
@@ -89,60 +89,60 @@ $( document ).ready(function() {
         current_row.find("input.merge-arrow-right").hide();
       }
     }
-	};
-	
-	var generate_merge_column = function(side, state, index){
-		var cellHTML = cellElements[index];
-		cellHTML.addClass(get_state_css(state));
-		var lastRow = $("#notebook-container-new").children().last();
-		var htmlClass = ".row-cell-merge-"+side;
-		lastRow.children(htmlClass).append(cellHTML);
-	};
-	
-	var get_state_css = function(state){
-		if(state == cellState[0]){
-			return "added-cell";
-		} else if ( state == cellState[1]){
-			return "deleted";
-		} else if ( state == cellState[2]){
-			return "changed";
-		} else {
-			return "";
-		} 
-	};
-	
-	var generate_merge_control_column = function(side) {
-		var mergeArrowClass = 'merge-arrow-left';
-		if(side == cellSide[0]){
-			return "<input value='->' data-cell-idx='0' class='merge-arrow-right' type='button'>";
-		} else {
-			return "<input value='<-' data-cell-idx='0' class='merge-arrow-left' type='button'>";
-		}		
-	};
-	
-	var generate_empty_merge_row = function() {
-		return "<div class='row'>"+
-					"<div class='row-cell-merge-local'></div>"+
-					"<div class='row-cell-merge-controls-local'>"+generate_merge_control_column("local")+"</div>"+
-					"<div class='row-cell-merge-remote'></div>"+
-					"<div class='row-cell-merge-controls-remote'>"+generate_merge_control_column("remote")+"</div>"+
-					"<div class='row-cell-merge-base'></div>"+
-				"</div>";
-	};
+    };
+    
+    var generate_merge_column = function(side, state, index){
+        var cellHTML = cellElements[index];
+        cellHTML.addClass(get_state_css(state));
+        var lastRow = $("#notebook-container-new").children().last();
+        var htmlClass = ".row-cell-merge-"+side;
+        lastRow.children(htmlClass).append(cellHTML);
+    };
+    
+    var get_state_css = function(state){
+        if(state == cellState[0]){
+            return "added-cell";
+        } else if ( state == cellState[1]){
+            return "deleted";
+        } else if ( state == cellState[2]){
+            return "changed";
+        } else {
+            return "";
+        } 
+    };
+    
+    var generate_merge_control_column = function(side) {
+        var mergeArrowClass = 'merge-arrow-left';
+        if(side == cellSide[0]){
+            return "<input value='->' data-cell-idx='0' class='merge-arrow-right' type='button'>";
+        } else {
+            return "<input value='<-' data-cell-idx='0' class='merge-arrow-left' type='button'>";
+        }        
+    };
+    
+    var generate_empty_merge_row = function() {
+        return "<div class='row'>"+
+                    "<div class='row-cell-merge-local'></div>"+
+                    "<div class='row-cell-merge-controls-local'>"+generate_merge_control_column("local")+"</div>"+
+                    "<div class='row-cell-merge-remote'></div>"+
+                    "<div class='row-cell-merge-controls-remote'>"+generate_merge_control_column("remote")+"</div>"+
+                    "<div class='row-cell-merge-base'></div>"+
+                "</div>";
+    };
 
-	var generate_notebook_container = function() {
-		return "<div class='container' id='notebook-container-new' style='display:inline'></div>";
-	};
-	
-	var generate_notebook_container_end_space = function() {
-		return "<div class='end_space'></div>";
-	};
-	
-	
-	init();
+    var generate_notebook_container = function() {
+        return "<div class='container' id='notebook-container-new' style='display:inline'></div>";
+    };
+    
+    var generate_notebook_container_end_space = function() {
+        return "<div class='end_space'></div>";
+    };
+    
+    
+    init();
 });
 
-	var remove_metadata = function(cells) {
+    var remove_metadata = function(cells) {
     for (var i = 0; i < cells.length; i++) {
       delete cells[i].metadata.state;
       delete cells[i].metadata.side;
