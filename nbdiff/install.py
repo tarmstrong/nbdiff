f36248c10442ad3c809a354e4495a66fac48a377
Tavish Armstrong <tavisharmstrong@gmail.com>
2013-11-23 23:59:38 -0500

    If installation isn't going well, abort and notify the user.

--- nbdiff/install.py
+++ nbdiff/install.py
@@ -3,6 +3,7 @@ import subprocess
 import re
 import os
 import shutil
+import sys
 
 
 def install():
@@ -12,11 +13,17 @@ def install():
     message_str = message.stderr.read()
     re_msgline = \
         re.compile(r'^.ProfileCre.*u\'(?P<profilepath>.*)ipython_config\.py.$')
-    profile_path = [
+    profile_paths = [
         re_msgline.match(line).groups()[0]
         for line in message_str.splitlines()
         if re_msgline.match(line)
-    ][0]
+    ]
+    if len(profile_paths) == 0:
+        sys.stderr.write("It looks like creating the ipython profile "
+            "didn't work. Maybe you've already installed it?\n")
+        sys.exit(-1)
+
+    profile_path = profile_paths[0]
     extension_copy_from = os.path.join(NBDIFF_PATH[0], 'extension/static')
     extension_copy_dest = os.path.join(profile_path, 'static')
     print extension_copy_from
