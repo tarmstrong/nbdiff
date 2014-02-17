b3858714fa5ba2a232498b9b5675e38a07b5fab0
bsplay <bsplay@gmail.com>
2014-02-17 02:10:20 -0500

    changes

--- nbdiff/adapter/vcs_adapter.py
+++ nbdiff/adapter/vcs_adapter.py
@@ -9,13 +9,13 @@ class VcsAdapter(object):
         raise NotImplementedError("Subclass must implement abstract method")
 
     def get_modified_notebooks(self, file_hooks):
-        unmerged_notebooks = []
+        modified_notebooks = []
         for item in file_hooks:
             if re.search('.ipynb$', item[2]):
                 three_tuple = (item[0], item[1], item[2])
-                unmerged_notebooks.append(three_tuple)
+                modified_notebooks.append(three_tuple)
 
-        return unmerged_notebooks
+        return modified_notebooks
 
     def get_unmerged_files(self):
         raise NotImplementedError("Subclass must implement abstract method")
