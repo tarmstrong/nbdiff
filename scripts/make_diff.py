af067798c2e939df96a3b545bdfd4b2ed8404d24
Tavish Armstrong <tavisharmstrong@gmail.com>
2014-03-02 17:34:42 -0500

    Rewrite make_diff.py

--- scripts/make_diff.py
+++ scripts/make_diff.py
@@ -14,6 +14,11 @@ import random
 import hashlib
 import sys
 
+SCRIPTS_DIR = os.path.realpath(os.path.dirname(__file__))
+EXAMPLE_NOTEBOOKS = os.path.join(SCRIPTS_DIR, 'example-notebooks/diff/')
+
+example_diff_notebooks = os.listdir(EXAMPLE_NOTEBOOKS)
+
 randpart = hashlib.sha1(str(random.random())).hexdigest()[:4]
 
 folder_name = "merge-diff-testfolder-{}".format(randpart)
@@ -21,16 +26,22 @@ os.mkdir(folder_name)
 os.chdir(folder_name)
 print subprocess.check_output('git init'.split())
 
-# create initial version of notebook
-with open('test.ipynb', 'w') as f:
-    f.write(open('../diff-example.ipynb').read())
 
-print subprocess.check_output('git add test.ipynb'.split())
+# create initial version of notebooks
+for nb in example_diff_notebooks:
+    target_fname = 'test-{id}.ipynb'.format(id=nb)
+    with open(target_fname, 'w') as f:
+        ipynb_path = os.path.join(EXAMPLE_NOTEBOOKS, nb, 'before.ipynb')
+        f.write(open(ipynb_path).read())
+        print subprocess.check_output(['git', 'add', target_fname])
+
 print subprocess.check_output(['git', 'commit', '-am', 'b'])
 
-# overwrite with changes.
-with open('test.ipynb', 'w') as f:
-    f.write(open('../diff-example-local.ipynb').read())
+for nb in example_diff_notebooks:
+    target_fname = 'test-{id}.ipynb'.format(id=nb)
+    with open(target_fname, 'w') as f:
+        ipynb_path = os.path.join(EXAMPLE_NOTEBOOKS, nb, 'after.ipynb')
+        f.write(open(ipynb_path).read())
 
 print 'Diffable notebook available in folder: ' + folder_name
 
