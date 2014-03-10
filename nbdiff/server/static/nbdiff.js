bf296f4c85bd59c6defcb3aa21dbb13408c57f13
bsplay <bsplay@gmail.com>
2014-03-10 03:09:07 -0400

    making jshint happy

--- nbdiff/server/static/nbdiff.js
+++ nbdiff/server/static/nbdiff.js
@@ -377,20 +377,20 @@ MergeRow.prototype = {
     moveRight: function () {
         console.log("move right");
         var cell = IPython.notebook.insert_cell_at_index(this._cells.local.cell.cell_type, 0);
-    	cell.fromJSON(this._cells.local.cell.toJSON());
-    	this._cells.base.cell.element.replaceWith(cell.element);
-    	this._cells.base.cell = cell;
-    	this._cells.base.cell.select()
+		cell.fromJSON(this._cells.local.cell.toJSON());
+		this._cells.base.cell.element.replaceWith(cell.element);
+		this._cells.base.cell = cell;
+		this._cells.base.cell.select();
         this._cells.base.cell.element.removeClass();
         this._cells.base.cell.element.addClass(this._cells.local.cell.element.attr("class"));
         this._cells.base.set_state(this._cells.local.state());
     },
     undo: function(cell_json, cell_class, old_state) {
-    	var cell = IPython.notebook.insert_cell_at_index(cell_json.cell_type, 0);
-    	cell.fromJSON(cell_json);
-    	this._cells.base.cell.element.replaceWith(cell.element);
-    	this._cells.base.cell = cell;
-    	this._cells.base.cell.select()
+		var cell = IPython.notebook.insert_cell_at_index(cell_json.cell_type, 0);
+		cell.fromJSON(cell_json);
+		this._cells.base.cell.element.replaceWith(cell.element);
+		this._cells.base.cell = cell;
+		this._cells.base.cell.select();
         this._cells.base.cell.element.removeClass();
         this._cells.base.cell.element.addClass(cell_class);
         this._cells.base.set_state(old_state);
