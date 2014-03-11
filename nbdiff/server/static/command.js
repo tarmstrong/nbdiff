8209371796fa21b23e21831824b4a0a08283eb98
bsplay <bsplay@gmail.com>
2014-03-10 21:01:06 -0400

    making jshint happy

--- nbdiff/server/static/command.js
+++ nbdiff/server/static/command.js
@@ -2,9 +2,9 @@
 
 var Invoker = (function () {
 	var _undo_command_stack = [];
-    var _undo_commands = [];
-    var storeAndExecute = function(command){
-		if(typeof _undo_commands[command.id] != "undefined")
+	var _undo_commands = [];
+	var storeAndExecute = function(command){
+		if(typeof _undo_commands[command.id] !== "undefined")
 		{
 			_undo_commands[command.id].push(command);
 		}
@@ -17,22 +17,23 @@ var Invoker = (function () {
 		command.execute();
 	};
 	var undo = function(id) {
-		if(typeof id != "undefined")
+		var command = null;
+		if(typeof id !== "undefined")
 		{
 			if(_undo_commands[id].length > 0) {
-	            var command = _undo_commands[id].pop();
-	            var index = _undo_command_stack.indexOf(command);
-	            _undo_command_stack.splice(index, 1);
-	            command.undo();
-	        }
-	        else {
-	            throw "Nothing to undo.";
-	        }
+				command = _undo_commands[id].pop();
+				var index = _undo_command_stack.indexOf(command);
+				_undo_command_stack.splice(index, 1);
+				command.undo();
+			}
+			else {
+				throw "Nothing to undo.";
+			}
 		}
 		else
 		{
 			if(_undo_command_stack.length > 0) {
-				var command = _undo_command_stack.pop();
+				command = _undo_command_stack.pop();
 				_undo_commands[command.id].pop();
 				command.undo();
 			}
@@ -40,20 +41,20 @@ var Invoker = (function () {
 				throw "Nothing to undo.";
 			}
 		}
-    };
-    return {
-        storeAndExecute: storeAndExecute,
-        undo: undo
-    };
+	};
+	return {
+		storeAndExecute: storeAndExecute,
+		undo: undo
+	};
 })();
 
 /* Base command class */
 function Command(merge_row) {
 	this.merge_row = merge_row;
-    this.old_classes = merge_row._cells.base.element().attr("class");
-    this.old_state = merge_row._cells.base.state();
-    this.id = merge_row.rowID;
-    this.cell_JSON = merge_row._cells.base.cell.toJSON();
+	this.old_classes = merge_row._cells.base.element().attr("class");
+	this.old_state = merge_row._cells.base.state();
+	this.id = merge_row.rowID;
+	this.cell_JSON = merge_row._cells.base.cell.toJSON();
 }
 
 function MoveLeftCommand(merge_row) {
@@ -64,15 +65,15 @@ MoveLeftCommand.prototype = Object.create(Command.prototype);
 
 /* The Command for Moving left - ConcreteCommand #1 */
 MoveLeftCommand.prototype = {
-    execute: function() {
-        this.merge_row.moveLeft();
-    },
-    undo: function() {
-    	var $button = $(this.merge_row._cells.base.element()).closest(".row").find('.row-cell-merge-controls-remote > input');
-        $button.removeClass("undo-button-remote");
-        $button.val("<-");
-        this.merge_row.undo(this.cell_JSON, this.old_classes, this.old_state);
-    }
+	execute: function() {
+		this.merge_row.moveLeft();
+	},
+	undo: function() {
+		var $button = $(this.merge_row._cells.base.element()).closest(".row").find('.row-cell-merge-controls-remote > input');
+		$button.removeClass("undo-button-remote");
+		$button.val("<-");
+		this.merge_row.undo(this.cell_JSON, this.old_classes, this.old_state);
+	}
 };
 
 function MoveRightCommand(merge_row) {
@@ -83,13 +84,13 @@ MoveRightCommand.prototype = Object.create(Command.prototype);
 
 /* The Command for Moving right - ConcreteCommand #2 */
 MoveRightCommand.prototype = {
-    execute: function() {
-        this.merge_row.moveRight();
-    },
-    undo: function() {
-    	var $button = $(this.merge_row._cells.base.element()).closest(".row").find('.row-cell-merge-controls-local > input');
-        $button.removeClass("undo-button-local");
-        $button.val("->");
-        this.merge_row.undo(this.cell_JSON, this.old_classes, this.old_state);
-    }
+	execute: function() {
+		this.merge_row.moveRight();
+	},
+	undo: function() {
+		var $button = $(this.merge_row._cells.base.element()).closest(".row").find('.row-cell-merge-controls-local > input');
+		$button.removeClass("undo-button-local");
+		$button.val("->");
+		this.merge_row.undo(this.cell_JSON, this.old_classes, this.old_state);
+	}
 };
