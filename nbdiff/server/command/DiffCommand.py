32df824a51c9b8934260d647bd7472f352a4c5aa
NightlyBuilds <richard_2001_year@hotmail.com>
2014-03-12 02:05:38 -0400

    Added Error Handling for a few Commands

--- nbdiff/server/command/DiffCommand.py
+++ nbdiff/server/command/DiffCommand.py
@@ -1,40 +1,66 @@
 from . import BaseCommand
-from flask import redirect
+from flask import redirect, render_template
 from ...notebook_parser import NotebookParser
 from ...notebook_diff import notebook_diff
 from nbdiff.server.database.nbdiffModel import nbdiffModel
+from werkzeug.exceptions import BadRequestKeyError
 import json
 import bitarray
+import IPython.nbformat.current as nbformat
 
 
 class DiffCommand(BaseCommand):
 
     def process(self, request, filename, db_session):
-        before = request.form['beforeJSON']
-        after = request.form['afterJSON']
-
+        errMsg = ""
         parser = NotebookParser()
+        
+        try:
+            before = request.form['beforeJSON']
+            after = request.form['afterJSON']
+        except BadRequestKeyError:
+            errMsg = """Invalid notebook Diff Request. <br/>
+                Please return to the home page and submit the request again."""
+            return render_template('Error.html', err=errMsg)
 
-        nb_before = parser.parseString(before)
-        nb_after = parser.parseString(after)
-        diffNotebook = notebook_diff(nb_before, nb_after)
+        try:
+            nb_before = parser.parseString(before)
+        except nbformat.NotJSONError:
+            errMsg = errMsg + "The Before notebook contains invalid JSON data. <br/>"
+        try:    
+            nb_after = parser.parseString(after)
+        except nbformat.NotJSONError:
+            errMsg = errMsg + "The After notebook contains invalid JSON data. <br/>"
+         
+        if len(errMsg) == 0:
+                
+            diffNotebook = notebook_diff(nb_before, nb_after)
 
-        #bitarray used to convert notebook to binary for BLOB
-        ba = bitarray.bitarray()
-        ba.fromstring(json.dumps(diffNotebook, indent=2))
-        
-        #object to be saved to database
-        obj = nbdiffModel(ba.to01())
-        
-        #add to database and commit it.
-        db_session.add(obj)
-        db_session.commit()
-        
-        #return the id of the object.
-        nb_id = obj.id 
-        
-        #redirect is used because we want user to have a easier url to return to.
-        return redirect("/Comparison/"+str(nb_id), code=302)
+            #bitarray used to convert notebook to binary for BLOB
+            ba = bitarray.bitarray()
+            ba.fromstring(json.dumps(diffNotebook, indent=2))
+            
+            #object to be saved to database
+            obj = nbdiffModel(ba.to01())
+            
+            #add to database and commit it.
+            try:
+                db_session.add(obj)
+                db_session.commit()
+            except:
+                db_session.rollback()
+                errMsg = """There was an unexpected error with the database. <br/>
+                    Please try again later. <br/>
+                    If this problem persists please contact administrator."""
+                return render_template('Error.html', err=errMsg)
+                
+            #return the id of the object.
+            nb_id = obj.id 
+            
+            #redirect is used because we want user to have a easier url to return to.
+            return redirect("/Comparison/"+str(nb_id), code=302)
+        else:
+            return render_template('Error.html', err=errMsg)
 
 
 def newInstance():
--- nbdiff/server/command/MergeCommand.py
+++ nbdiff/server/command/MergeCommand.py
@@ -1,42 +1,72 @@
 from . import BaseCommand
-from flask import redirect
+from flask import redirect, render_template
 from ...notebook_parser import NotebookParser
 from ...merge import notebook_merge
 from nbdiff.server.database.nbdiffModel import nbdiffModel
+from werkzeug.exceptions import BadRequestKeyError
 import json
 import bitarray
+import IPython.nbformat.current as nbformat
+
 
 class MergeCommand(BaseCommand):
 
     def process(self, request, filename, db_session):
-    
-        local = request.form['localJSON']
-        remote = request.form['remoteJSON']
-        base = request.form['baseJSON']
-
+        
+        errMsg = ""
         parser = NotebookParser()
+        
+        try:
+            local = request.form['localJSON']
+            remote = request.form['remoteJSON']
+            base = request.form['baseJSON']
+        except BadRequestKeyError:
+            errMsg = """Invalid notebook Merge Request. <br/>
+                Please return to the home page and submit the request again."""
+            return render_template('Error.html', err=errMsg)
 
-        nb_local = parser.parseString(local)
-        nb_base = parser.parseString(base)
-        nb_remote = parser.parseString(remote)
-        mergedNotebook = notebook_merge(nb_local, nb_base, nb_remote)
-           
-        #bitarray used to convert notebook to binary for BLOB
-        ba = bitarray.bitarray()
-        ba.fromstring(json.dumps(mergedNotebook, indent=2))
+        try:
+            nb_local = parser.parseString(local)
+        except nbformat.NotJSONError:
+            errMsg = errMsg + "The Local notebook contains invalid JSON data. <br/>"
+        try:    
+            nb_base = parser.parseString(base)
+        except nbformat.NotJSONError:
+            errMsg = errMsg + "The Base notebook contains invalid JSON data. <br/>"
+        try:    
+            nb_remote = parser.parseString(remote)
+        except nbformat.NotJSONError:
+            errMsg = errMsg + "The Remote notebook contains invalid JSON data. <br/>"
         
-        #object to be saved to database
-        obj = nbdiffModel(ba.to01())
+        if len(errMsg) == 0:
         
-        #add to database and commit it.
-        db_session.add(obj)
-        db_session.commit()
-        
-        #return the id of the object.
-        nb_id = obj.id 
-        
-        #redirect is used because we want user to have a easier url to return to.
-        return redirect("/Comparison/"+str(nb_id), code=302)
+            mergedNotebook = notebook_merge(nb_local, nb_base, nb_remote)
+               
+            #bitarray used to convert notebook to binary for BLOB
+            ba = bitarray.bitarray()
+            ba.fromstring(json.dumps(mergedNotebook, indent=2))
+            
+            #object to be saved to database
+            obj = nbdiffModel(ba.to01())
+            
+            #add to database and commit it.
+            try:
+                db_session.add(obj)
+                db_session.commit()
+            except:
+                db_session.rollback()
+                errMsg = """There was an unexpected error with the database. <br/>
+                    Please try again later. <br/>
+                    If this problem persists please contact administrator."""
+                return render_template('Error.html', err=errMsg)
+            
+            #return the id of the object.
+            nb_id = obj.id 
+            
+            #redirect is used because we want user to have a easier url to return to.
+            return redirect("/Comparison/"+str(nb_id), code=302)
+        else:
+            return render_template('Error.html', err=errMsg)
 
 
 def newInstance():
