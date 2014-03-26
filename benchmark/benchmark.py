0f23654b6d01eeeb78f261fae03e4bb9e669ba03
Tavish Armstrong <tavisharmstrong@gmail.com>
2014-03-26 00:18:03 -0400

    Output commit and timestamp in benchmark script

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -7,8 +7,16 @@ def run_diff_once():
 
     subprocess.call(['nbdiff', '--check', f1, f2])
 
-
 if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('commit')
+    args = parser.parse_args()
     t = timeit.Timer('run_diff_once()', setup='from __main__ import run_diff_once')
-    print t.repeat(repeat=40, number=1)
+    measurements = t.repeat(repeat=40, number=1)
+    import csv
+    import sys
+    import arrow
+    row = [args.commit, arrow.now().timestamp] + measurements
+    csv.writer(sys.stdout).writerow(row)
 
