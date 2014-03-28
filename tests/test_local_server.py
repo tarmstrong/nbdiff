e2e41045f53fe519dc5b60c07a5f8efa0487a18e
Tavish Armstrong <tavisharmstrong@gmail.com>
2014-03-28 15:46:05 -0400

    Local server tests passing.

--- tests/test_local_server.py
+++ tests/test_local_server.py
@@ -1,38 +1,24 @@
-from flask import request
-import IPython
-
 from nbdiff.server.local_server import (
-    NbFlask,
     app,
-    nbdiff_static,
-    home,
-    notebookjson,
-    shutdown,
 )
- 
-callback = True
-
-def fake_callback(contents, filename):
-    callback = True
-
 
 def test_home():
     client = app.test_client()
     result = client.get('/1')
     assert result.status_code == 200
- 
- 
+
+
 def test_notebookjson():
     client = app.test_client()
     app.add_notebook({'metadata': {}}, 'foo.ipynb')
     result = client.get('/notebooks/test_notebook0')
     assert result.status_code == 200
 
+    def fake_callback(contents, filename):
+        fake_callback.called = True
+
     app.shutdown_callback(fake_callback)
     contents = ''
     result = client.put('/notebooks/test_notebook0', contents)
-    filename = 'hello.ipynb'
-    app.shutdown(result.data, filename)
-    assert callback
-
-    
+    assert result.data == "", result
+    assert fake_callback.called
