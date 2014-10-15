0032ff2c3a3df3726693ef70dc0472b48bf7d524
bpipev <bobipipev@hotmail.com>
2014-10-15 14:51:46 -0400

    Fixed hg_tests

--- tests/test_hg_adapter.py
+++ tests/test_hg_adapter.py
@@ -1,50 +1,56 @@
 import hglib
 import os
+import unittest
 
-from nbdiff.adapter import hg_adapter as hg
+from nbdiff.adapter.hg_adapter import HgAdapter
 from testfixtures import tempdir, compare
 
-
-@tempdir()
-def test_get_modified_notebooks_empty(d):
-    d.makedir('output')
-    os.chdir(os.path.join(d.path, 'output'))
-    hglib.init()
-    adapter = hg.HgAdapter()
-    result = adapter.get_modified_notebooks()
-    assert result == []
-
-
-@tempdir()
-def test_get_modified_notebooks(d):
-    os.chdir(d.path)
-    client = hglib.init()
-    client.open()
-    d.makedir('first')
-    d.makedir('first/second')
-    path = d.write('first/second/1.ipynb', 'initial')
-    compare(d.read('first/second/1.ipynb'), 'initial')
-    client.add('first/second/1.ipynb')
-    client.commit("message")
-    file = open(path, 'w')
-    file.write("modified")
-    file.close()
-    compare(d.read('first/second/1.ipynb'), 'modified')
-    os.chdir(os.path.join(d.path, 'first'))
-
-    adapter = hg.HgAdapter()
-    result = adapter.get_modified_notebooks()
-    assert len(result) == 1
-    compare(result[0][0].read(), 'modified')
-    compare(result[0][1].read(), 'initial')
-    compare(result[0][2], 'first/second/1.ipynb')
-
-
-@tempdir()
-def test_get_unmerged_notebooks_empty(d):
-    d.makedir('output')
-    os.chdir(os.path.join(d.path, 'output'))
-    hglib.init()
-    adapter = hg.HgAdapter()
-    result = adapter.get_unmerged_notebooks()
-    assert result == []
+class TestSequenceFunctions(unittest.TestCase):
+    def setUp(self):
+        self.original_path = os.getcwd()
+        
+    def tearDown(self):
+        os.chdir(self.original_path)
+    
+    @tempdir()
+    def test_get_modified_notebooks_empty(self, d):
+        os.chdir(os.path.join(d.path))
+        hglib.init()
+        adapter = HgAdapter()
+        result = adapter.get_modified_notebooks()
+        self.assertTrue(result == []) 
+    
+    
+    @tempdir()
+    def test_get_modified_notebooks(self, d):
+        original_path = os.getcwd()
+        os.chdir(d.path)
+        client = hglib.init()
+        client.open()
+        d.makedir('first')
+        d.makedir('first/second')
+        path = d.write('first/second/1.ipynb', 'initial')
+        self.assertEqual(d.read('first/second/1.ipynb'), 'initial')
+        client.add('first/second/1.ipynb')
+        client.commit("message")
+        file = open(path, 'w')
+        file.write("modified")
+        file.close()
+        self.assertEqual(d.read('first/second/1.ipynb'), 'modified')
+        os.chdir(os.path.join(d.path, 'first'))
+    
+        adapter = HgAdapter()
+        result = adapter.get_modified_notebooks()
+        self.assertTrue(len(result) == 1)
+        self.assertEqual(result[0][0].read(), 'modified')
+        self.assertEqual(result[0][1].read(), 'initial')
+        self.assertEqual(result[0][2], 'first/second/1.ipynb')
+    
+    
+    @tempdir()
+    def test_get_unmerged_notebooks_empty(self, d):
+        os.chdir(os.path.join(d.path))
+        hglib.init()
+        adapter = HgAdapter()
+        result = adapter.get_unmerged_notebooks()
+        self.assertTrue(result == []) 
